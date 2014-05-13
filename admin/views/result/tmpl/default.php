2a07c6e6682b9ef86754ffaa4f319e50fb0fd07b
Antti Kekki <antti.kekki@gmail.com>
2014-05-13 15:56:51 +0300

    Improved election results view table layout

--- admin/views/result/tmpl/default.php
+++ admin/views/result/tmpl/default.php
@@ -4,59 +4,68 @@
 
 <h2><?php echo JText::_( 'Result for' ) ." ". $this->statistics->election_name; ?></h2>
 
-<div>
-	<table cellspacing="5">
-		<tr>
-			<td><b><?php echo JText::_( 'Total number of voters' ); ?>:</b></td>
-			<td><?php echo $this->statistics->voter_total; ?></td>
-		</tr>
-		<tr>
-			<td><b><?php echo JText::_( 'Voters who voted' ); ?>:</b></td>
-			<td><?php echo $this->statistics->voters_who_voted; ?></td>
-		</tr>
-		<tr>
-			<td><b><?php echo JText::_( 'Voter percentage' ); ?>:</b></td>
-			<td><?php echo $this->statistics->voted_percentage; ?> %</td>
-		</tr>
-	</table>
+<br><br>
+
+<div class="row-fluid">
+  <div class="span3">
+    <table class="table">
+      <tr>
+        <td><b><?php echo JText::_( 'Total number of voters' ); ?>:</b></td>
+        <td><?php echo $this->statistics->voter_total; ?></td>
+      </tr>
+      <tr>
+        <td><b><?php echo JText::_( 'Voters who voted' ); ?>:</b></td>
+        <td><?php echo $this->statistics->voters_who_voted; ?></td>
+      </tr>
+      <tr>
+        <td><b><?php echo JText::_( 'Voter percentage' ); ?>:</b></td>
+        <td><?php echo $this->statistics->voted_percentage; ?> %</td>
+      </tr>
+    </table>
+  </div>
 </div>
 
+<br><br>
 
-<div>
-	<table cellspacing="5">
-		<tr>
-			<td><b><?php echo JText::_( 'Option number' ); ?></b></td>
-			<td><b><?php echo JText::_( 'Option name' ); ?></b></td>
-			<td><b><?php echo JText::_( 'Option votes' ); ?></b></td>
-			
-			<?php 
-			if($this->election_type_id == 2) {
-				?><td><b><?php echo JText::_( 'Election list name' ); ?></b></td>
-				  <td><b><?php echo JText::_( 'Election list votes' ); ?></b></td><?php
-			}
-			?>
-		</tr>
-		
-		<?php 
-		for ($i=0, $n=count( $this->results ); $i < $n; $i++) {		
-			$row = &$this->results[$i]; ?>
-			
-			<tr>
-				<td><?php echo $row->option_number; ?></td>
-				<td><?php echo $row->name; ?></td>
-				<td><?php echo $row->votes; ?></td>
-				
-				<?php 
-				if($this->election_type_id == 2) {
-					?><td><?php echo $row->list_name; ?></td>
-					  <td><?php echo $row->list_votes; ?></td><?php
-				}
-				?>
-				
-			</tr>
-			
-		<?php } ?>
-	</table>
+<div class="row-fluid">
+  <div class="span5">
+    <table class="table">
+      <thead>
+        <tr>
+          <th><?php echo JText::_( 'Option number' ); ?></th>
+          <th><?php echo JText::_( 'Option name' ); ?></th>
+          <th><?php echo JText::_( 'Option votes' ); ?></th>
+          
+          <?php 
+          if($this->election_type_id == 2) {
+            ?><th><?php echo JText::_( 'Election list name' ); ?></th>
+              <th><?php echo JText::_( 'Election list votes' ); ?></th><?php
+          }
+          ?>
+        </tr>
+      </thead>
+      <tbody>
+        <?php 
+        for ($i=0, $n=count( $this->results ); $i < $n; $i++) {		
+          $row = &$this->results[$i]; ?>
+          
+          <tr>
+            <td><?php echo $row->option_number; ?></td>
+            <td><?php echo $row->name; ?></td>
+            <td><?php echo $row->votes; ?></td>
+            
+            <?php 
+            if($this->election_type_id == 2) {
+              ?><td><?php echo $row->list_name; ?></td>
+                <td><?php echo $row->list_votes; ?></td><?php
+            }
+            ?>
+            
+          </tr>
+        <?php } ?>
+      </tbody>
+    </table>
+  </div>
 </div>
 
 
