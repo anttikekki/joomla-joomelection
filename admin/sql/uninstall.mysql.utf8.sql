10ac9a973162a4fc3f5ee1b7182fca29612c8122
anttikekki <anttikekki@Antti-MacBook-Pro.local>
2014-10-01 18:40:29 +0300

    Updated uninstall script

--- admin/sql/uninstall.mysql.utf8.sql
+++ admin/sql/uninstall.mysql.utf8.sql
@@ -1,5 +1,8 @@
-DROP TABLE IF EXISTS `#__election`;
-DROP TABLE IF EXISTS `#__option`;
-DROP TABLE IF EXISTS `#__vote`;
-DROP TABLE IF EXISTS `#__election_voter`;
-DROP TABLE IF EXISTS `#__voter`;
+DROP TABLE IF EXISTS `#__joomelection_election`;
+DROP TABLE IF EXISTS `#__joomelection_election_type`;
+DROP TABLE IF EXISTS `#__joomelection_list`;
+DROP TABLE IF EXISTS `#__joomelection_option`;
+DROP TABLE IF EXISTS `#__joomelection_vote`;
+DROP TABLE IF EXISTS `#__joomelection_election_voter_status`;
+DROP TABLE IF EXISTS `#__joomelection_voter`;
+DROP TABLE IF EXISTS `#__joomelection_translation`;
